a6f903655983464cfc61e0ea2b445757fba86291
dyanfelipe <dyansilva@outlook.com>
2020-03-01 21:14:50 -0300

    list and style of page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,39 @@
 import React, { Component } from "react";
 
-import { FaGithubAlt, FaPlus } from "react-icons/fa";
+import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
 
-import { Container, Form, SubmitButton } from "./styles";
+import api from "../../services/api";
+
+import { Container, Form, SubmitButton, List } from "./styles";
 
 export default class Main extends Component {
   state = {
-    newRepo: ""
+    newRepo: "",
+    respositories: [],
+    loading: false
   };
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
-    console.log(this.state.newRepo);
+    this.setState({ loading: true });
+    const { newRepo, respositories } = this.state;
+    const response = await api.get(`/repos/${newRepo}`);
+    const data = {
+      name: response.data.full_name
+    };
+    this.setState({
+      respositories: [...respositories, data],
+      newRepo: "",
+      loading: false
+    });
   };
 
   render() {
-    const { newRepo } = this.state;
+    const { newRepo, respositories, loading } = this.state;
     return (
       <Container>
         <FaGithubAlt />
@@ -31,10 +45,23 @@ export default class Main extends Component {
             value={newRepo}
             onChange={this.handleInputChange}
           />
-          <SubmitButton type="submit">
-            <FaPlus color="#FFF" size={14} />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {respositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <a href="">Detalhes</a>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
